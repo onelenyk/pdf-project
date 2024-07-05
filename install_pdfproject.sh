e56e4cd42a6275d64f1bfc9ff61a721fa0b8bf03
Nazar Lenyk <nazar.lenyk@vanongo.com>
2024-07-06 02:34:41 +0300

    feat: update install_pdfproject.sh and add pdfproject script

--- install_pdfproject.sh
+++ install_pdfproject.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
+# Determine the directory where the script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_DIR="$SCRIPT_DIR/pdf"
 # Variables
-VERSION=0.0.6
-TARGET_DIR="/usr/local/bin/pdfproject"
-TARGET_SCRIPT="$TARGET_DIR/pdfproject"
-
-# Function to build JAR URL
-build_jar_url() {
-    JAR_URL="https://jitpack.io/com/github/onelenyk/pdf-project/v$VERSION/pdf-project-v$VERSION.jar"
-    JAR_PATH="$TARGET_DIR/pdf-project-v$VERSION.jar"
-}
+VERSION="0.0.6" # This will be updated by the modify_setup.sh script
+JAR_NAME="pdfproject.jar"
+JAR_PATH="$PROJECT_DIR/$JAR_NAME"
+JAR_URL="https://jitpack.io/com/github/onelenyk/pdf-project/$VERSION/pdf-project-$VERSION.jar"
+TARGET_SCRIPT="$PROJECT_DIR/pdfproject"
 
 # Function to check if JAR file exists
 check_jar_exists() {
@@ -24,7 +23,7 @@ check_jar_exists() {
 # Function to download the JAR file from JitPack repository
 download_jar() {
     echo "Downloading JAR file from $JAR_URL..."
-    sudo mkdir -p "$TARGET_DIR"
+    sudo mkdir -p "$PROJECT_DIR"
     sudo curl -L -o "$JAR_PATH" "$JAR_URL"
 }
 
@@ -47,15 +46,16 @@ java -jar \"\$JAR_PATH\" \"\$@\"
     # Make the script executable
     sudo chmod +x "$TARGET_SCRIPT"
 
+    echo "Script created at $TARGET_SCRIPT and made executable."
+
     # Create a symlink to the script in /usr/local/bin for easy access
+    echo "Creating symlink to /usr/local/bin/pdfproject..."
     sudo ln -sf "$TARGET_SCRIPT" /usr/local/bin/pdfproject
 
-    echo "Script created at $TARGET_SCRIPT and made executable."
+    echo "Symlink created."
 }
 
 # Main script logic
-build_jar_url
-
 if check_jar_exists; then
     echo "JAR file is already present. No need to download."
 else
